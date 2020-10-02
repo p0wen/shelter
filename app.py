691f6ef37edec85f60b421e52bc3791e8ff0cd26
p0wen <Philly.e@hotmail.de>
2020-10-02 10:02:24 +0000

    feat: set up PyMongo and templating

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask('__name__')
 
-app.config.from_object("env")
+app.config.from_object('env')
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
 def welcome():
     return 'Welcome Stranger - Lets do this!'
+
+
+@app.route('/get_gear')
+def get_gear():
+    return render_template("gallery.html", gear_collection=mongo.db.gear.find())
 
 
 if __name__ == '__main__':
