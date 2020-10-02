373f61ada2e18d1a9aa06687aa983ec4e501bec3
p0wen <Philly.e@hotmail.de>
2020-10-02 08:43:31 +0000

    feat: setup connection to mongodb

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import os
-from flask import Flask 
+from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask('__name__')
 
+app.config.from_object("env")
 
 @app.route('/')
 def welcome():
-    return 'Welcome Stranger - Lets do this!' 
+    return 'Welcome Stranger - Lets do this!'
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
