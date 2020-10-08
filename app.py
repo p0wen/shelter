5498c49c8b09593c6f6f2125989847c8366a24dc
p0wen <Philly.e@hotmail.de>
2020-10-08 08:23:17 +0000

    fix: remove , at end of app.secret assignment

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def about():
 
 
 if __name__ == '__main__':
-    app.secret_key = os.environ.get('SECRET_KEY'),
+    app.secret_key = os.environ.get('SECRET_KEY')
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
